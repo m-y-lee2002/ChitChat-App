3506b518a3e7667e9ed10f515c2b1a4c0f6105a1
Mike <mylee13845@gmail.com>
2024-04-11 22:50:09 -0400

    Connected MongoDB to Server side & set port listener

--- server/index.js
+++ server/index.js
@@ -1,1 +1,19 @@
-console.log("hello");
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+
+const uri = "mongodb+srv://admin:admin123@friendscluster.x4xg0ne.mongodb.net/?retryWrites=true&w=majority&appName=friendsCluster";
+ 
+async function connect(){
+    try{
+        await mongoose.connect(uri);
+        console.log("Connected to MongoDB");
+    }catch(error){
+        console.error(error);
+    }
+}
+connect();
+
+app.listen(8000, ()=>{
+    console.log("Server connected on port 8000");
+});
