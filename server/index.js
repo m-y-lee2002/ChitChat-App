bf2f947fde9b5cf6633ceb19d792cb0c6053f92f
Mike <mylee13845@gmail.com>
2024-04-13 22:56:24 -0400

    Included express.json() middleware to parse json & made post request

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const app = express();
 const User = require('./schema_Models/user.js');
 const Friends = require('./schema_Models/friend.js');
+app.use(express.json());
 
 const uri = "mongodb+srv://admin:admin123@friendscluster.x4xg0ne.mongodb.net/ChitChatDB?retryWrites=true&w=majority&appName=friendsCluster";
 
@@ -26,4 +27,11 @@ app.listen(8000, () => {
 app.get('/api/get', async(req, res)=>{
     const result = await User.find({});
     res.send(result);
+})
+
+app.post('/api/post', async(req,res)=>{
+    const request_info = req.body;
+    const newUser = new User(request_info); 
+    await newUser.save();
+    res.status(201);
 })
