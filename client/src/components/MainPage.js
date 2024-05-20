4f2580cdaa50671f9fa995e1aa88098ba4e1a728
Mike <mylee13845@gmail.com>
2024-05-19 21:07:40 -0400

    Reformatted code to use component rendering life cycle and allowed for all friends to display for a given user

--- client/src/components/MainPage.js
+++ client/src/components/MainPage.js
@@ -1,17 +1,46 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';  
 import { getAPI } from '../api/getAPI';
-import {SHA1} from 'crypto-js';
-function MainPage(){
-    const user = localStorage.getItem('username');
-    console.log(user);
-    return(
-        <div className="mainpage">
-            <h1>{user}'s Friends</h1>
 
-            <button>Add Friend</button>
-        </div>
-    );
+class MainPage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            friends: [], 
+            user: localStorage.getItem('username') 
+        };
+    }
+
+    componentDidMount() {
+        const { user } = this.state;
+        if (user) {
+            getAPI('/friends/' + user)
+                .then(data => {
+                    this.setState({ friends: data }); 
+                    data.forEach(friend => console.log(friend.fname));
+                })
+                .catch(error => {
+                    console.log(error);
+                });
+        }
+    }
+
+    render() {
+        const { friends, user } = this.state;
+        return (
+            <div className="mainpage">
+                <button>Back</button>
+                <h1>{user}'s Friends</h1>
+                <ul>
+                    {friends.map(friend => (
+                      <li key={friend.fid}>{friend.fname}<button>Delete</button></li>
+                    ))}
+                </ul>
+                <button>Add Friend</button>
+            </div>
+        );
+    }
 }
+
 export default MainPage;
