cdc8e63e197727f771f93f84bbe0e170233896d4
Mike <mylee13845@gmail.com>
2024-05-20 17:11:40 -0400

    Refactored code to use class based component rendering

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -1,53 +1,81 @@
-import React, { useState } from 'react';
+import React, { Component} from 'react';
 import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 import { getAPI } from '../api/getAPI';
 import {SHA1} from 'crypto-js';
 
-function Login() {
-    const navigate = useNavigate();
-    const [username, setUsername] = useState("name");
-    const [password, setPassword] = useState("name");
-    const submitHandler = (event)=>{
-      event.preventDefault();
-      let found = false;
-        getAPI('')
-        .then(data=>{
-            data.forEach((tuple)=>{
-              if(username === tuple.username && SHA1(password).toString() === tuple.password){
-                found = true;
-                localStorage.setItem('username',username);
-                navigate('./mainpage');
-              }
-            });
-            if(found){
-              console.log("User Found");
 
-            }else{
-              console.log(SHA1(password).toString());
-              alert("Invalid Login");
-            }
-        }).catch(error => {
-          console.error(error);
-        });
+class Login extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      username: '',
+      password: ''
     };
-    return (
+    
+    this.setUsername = this.setUsername.bind(this);
+    this.setPassword = this.setPassword.bind(this);
+    this.submitHandler = this.submitHandler.bind(this);
+  }
+
+  setUsername(event){
+    this.setState({username: event.target.value});
+  }
+
+  setPassword(event){
+    this.setState({password:event.target.value});
+  }
+
+  submitHandler(event) {
+    event.preventDefault();
+    let found = false;
+    const { username, password } = this.state;
+
+    getAPI('')
+      .then(data => {
+        data.forEach((tuple) => {
+          if (username === tuple.username && SHA1(password).toString() === tuple.password) {
+            found = true;
+            localStorage.setItem('username', username);
+            this.props.navigate('/mainpage'); 
+          }
+        });
+        if (found) {
+          console.log("User Found");
+        } else {
+          console.log(SHA1(password).toString());
+          alert("Invalid Login");
+        }
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
+
+  render(){
+    return(
       <div className="login">
-        <header className="login-header">
-          <h1>Login Page</h1>
-        </header>
-        <form onSubmit={submitHandler}>
-            Username: <input type="text" onChange={(event)=>{setUsername(event.target.value)}}/>
-            <br/>
-            <br/>
-            Password: <input type="password" onChange={(event)=>{setPassword(event.target.value)}}/>
-            <br/>
-            <button type="submit">Submit</button>
-        </form>
-        <Link to="/register">create an account</Link> 
+      <header className="login-header">
+        <h1>Login Page</h1>
+      </header>
+      <form onSubmit={this.submitHandler}>
+          Username: <input type="text" onChange={this.setUsername}/>
+          <br/>
+          <br/>
+          Password: <input type="password" onChange={this.setPassword}/>
+          <br/>
+          <button type="submit">Submit</button>
+      </form>
+      <Link to="/register">create an account</Link> 
       </div>
+
     );
   }
-  
-  export default Login;
+}
+
+function PageNavigation(props){
+  const navigate = useNavigate();
+  return <Login{...props} navigate={navigate}/>;
+}
+export default PageNavigation;
   
