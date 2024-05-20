16ab9996fd82d2f8276563464b25871f214a4cc1
L29n <jim.xasdf@gmail.com>
2024-05-20 14:07:27 -0400

    Added navbar, and coverr page

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,17 @@
 import Login from "./components/login.js";
 import MainPage from "./components/MainPage.js";
 import Register from "./components/Register.js";
+import CoverPage from "./components/CoverPage.js"
+import NavBar from "./components/NavBar.js"
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
       <Router>
+          <NavBar/>
           <Routes>
-              <Route path = "/" element = {<Login/>}/>
+              <Route path = "/" element = {<CoverPage/>}/>
+              <Route path = "/login" element = {<Login/>}/>
               <Route path = "/mainpage" element = {<MainPage/>}/>
               <Route path = "/register" element = {<Register/>}/>
           </Routes>
---
+++ client/src/components/CoverPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class CoverPage extends Component{
+    render(){
+        return(
+            <>
+                <h1>Cover Page</h1>
+            </>
+        )
+    }
+}
+export default CoverPage;
