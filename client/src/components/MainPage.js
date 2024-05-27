a7d74d4b2bcad705b979fff09f52ccffd3f154e9
Mike <mylee13845@gmail.com>
2024-05-26 23:50:37 -0400

    Created delete friend functionality

--- client/src/components/MainPage.js
+++ client/src/components/MainPage.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';  
 import { getAPI } from '../api/getAPI';
+import { deleteAPI } from '../api/deleteAPI';
 
 class MainPage extends Component {
     constructor(props) {
@@ -10,9 +11,21 @@ class MainPage extends Component {
             friends: [], 
             user: localStorage.getItem('username') 
         };
+        this.logOut = this.logOut.bind(this);
+        this.deleteFriend = this.deleteFriend.bind(this);
+        this.fetchFriends = this.fetchFriends.bind(this);
     }
-
-    componentDidMount() {
+    logOut(){
+        localStorage.clear();
+        this.props.navigate('../login');
+    }
+    deleteFriend(targetID){
+        console.log(targetID);
+        deleteAPI('/friends/' + targetID);
+        this.fetchFriends();
+        //this.props.navigate('../mainpage');
+    }
+    fetchFriends(){
         const { user } = this.state;
         if (user) {
             getAPI('/friends/' + user)
@@ -23,18 +36,21 @@ class MainPage extends Component {
                 .catch(error => {
                     console.log(error);
                 });
-        }
+        }    
+    }
+    componentDidMount() {
+        this.fetchFriends();
     }
 
     render() {
         const { friends, user } = this.state;
         return (
             <div className="mainpage">
-                <button>Back</button>
+                <button onClick={this.logOut}>Back</button>
                 <h1>{user}'s Friends</h1>
                 <ul>
                     {friends.map(friend => (
-                      <li key={friend.fid}>{friend.fname}<button>Delete</button></li>
+                      <li key={friend.fid}>{friend.fname}<button onClick={() => this.deleteFriend(friend.fid)}>Delete</button></li>
                     ))}
                 </ul>
                 <button>Add Friend</button>
@@ -43,4 +59,9 @@ class MainPage extends Component {
     }
 }
 
-export default MainPage;
+function PageNavigation(props){
+    const navigate = useNavigate();
+    return <MainPage{...props} navigate={navigate}/>;
+  }
+  export default PageNavigation;
+    
